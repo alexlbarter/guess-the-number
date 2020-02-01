47f71fc1f8dca96e48359c7b784b2041a3e1f720
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-02-01 17:40:41 +0000

    added frames

--- ui.py
+++ ui.py
@@ -1,6 +1,12 @@
-import tkinter
+import tkinter as tk
 
-# Test
-window = tkinter.Tk()
+
+window = tk.Tk()
 window.title("Guess The Number")
+window.geometry("300x250")
+
+header_frame = tk.Frame(window)
+header_frame.pack()
+heading = tk.Label(header_frame, text="Guess The Number").pack()
+
 window.mainloop()
